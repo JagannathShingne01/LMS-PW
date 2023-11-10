badcab07925306264606fae0c4755f94bb1391e8
Jagannath Shingne <133676629+JagannathShingne01@users.noreply.github.com>
2023-11-10 20:56:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: [process.https://coursify-dun.vercel.app],
     credentials: true
 }));
 
